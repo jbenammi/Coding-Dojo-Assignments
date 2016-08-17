7188c9e5c3ca4dee654ef3a336f5ef2503a09a7a
Nicholis Muller <nicholis.muller@gmail.com>
2016-08-17 17:38:39 +0200

    Add animation and background color state toggle

--- jQuery_07/js/scripts.js
+++ jQuery_07/js/scripts.js
@@ -1,34 +1,32 @@
-
-
 $(document).ready(function () {
     console.log("jQuery on tap");
     $("#sortable").sortable();
     $("#sortable").disableSelection();
     var cache = $("#sortable").html();
-    
+
     $("#reset").click(function(){
         $("#sortable").html(cache).sortable("refresh");
+        $("body").css("background", "#7abcff").hide().fadeIn("slow");
     });
-    
-    
+
     $("#check").click(function(){
-         
+
     var order = $("#sortable").sortable("toArray");
     var counter = 0;
-    
+
         for(var i = 0; i < order.length; i++){
 
            if( parseInt(order[i]) < parseInt(order[i+1]) ) {
                counter ++
           }
         }
-   
+
         if(counter == 4){
+
             alert("Well done, the image is in order!")
-
-
+            $("body").css("background", "#66cdaa").hide().fadeIn("slow");
         }else{
             alert("Keep going, not there yet!")
-        }   
+        }
     });
 });
