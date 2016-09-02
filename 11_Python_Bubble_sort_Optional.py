b6905ec73762a35de2aaa6503b68ce2ed083b048
Nic M <njmuller@outlook.com>
2016-09-02 15:35:09 +0200

    Add time module to test efficiency

--- 11_Python_Bubble_sort_Optional.py
+++ 11_Python_Bubble_sort_Optional.py
@@ -1,7 +1,9 @@
 import random
+from time import time
+
+start_time = time()
 
 def bubble_sort(lst):
-
 
     idx = range(len(lst) -1)
     while sorted(lst) != lst:
@@ -19,3 +21,11 @@ def rand(num):
     return random_list
 
 bubble_sort(rand(100))
+
+end_time = time()
+time_taken = end_time - start_time # time_taken is in seconds
+
+print("\n")
+print("-"*80)
+print("This bubble sort algorithm took: {} seconds to complete.".format(time_taken))
+print("-"*80)
