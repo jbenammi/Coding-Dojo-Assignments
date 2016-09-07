45a43c90cc63766fc51cabd91aacc61263eb3603
Nic M <njmuller@outlook.com>
2016-09-07 14:42:52 +0200

    09_python_names

---
+++ 09_Python_Names.py
@@ -0,0 +1,30 @@
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+
+
+for dictionary in students:
+  print ' '.join(dictionary.values())
+
+
+users = {
+ 'Students': [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+counter = 0
+for key, data in users.iteritems():
+  print(key)
+  for value in data:
+    counter +=1
+    print counter, "-", ' '.join(value.values()), "-", len(''.join(value.values()))
